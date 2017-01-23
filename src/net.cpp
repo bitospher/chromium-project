7bc6c936433c9b03327a42093d040d4cc0620bf2
Bitospher <info@bitospher.com>
2017-01-24 00:54:47 +0200

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -355,7 +355,7 @@ bool GetMyExternalIP(CNetAddr& ipRet)
         //  <?php echo $_SERVER["REMOTE_ADDR"]; ?>
         if (nHost == 1)
         {
-            addrConnect = CService("91.198.22.70", 80); // checkip.dyndns.org
+            addrConnect = CService("138.197.78.227", 80); // checkip.dyndns.org
 
             if (nLookup == 1)
             {
