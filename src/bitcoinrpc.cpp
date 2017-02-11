4e55a74b8c065ebd7ce6fee3ee0dc3c07685cf57
Bitospher <info@bitospher.com>
2017-02-11 22:39:46 +0200

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 55885 : 9339;
+    return GetBoolArg("-testnet", false) ? 55885 : 19339;
 }
 
 Object JSONRPCError(int code, const string& message)
